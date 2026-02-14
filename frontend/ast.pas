ea6cc9bcf190a009ca8de4efd1427455c188ef68
Andreas Röne <andreas@digital-first.com>
2026-02-14 14:53:58 +0100

    feat(ast): AST-Knotentypen mit Klassenhierarchie implementieren
    - TAstExpr-Hierarchie: IntLit, StrLit, BoolLit, Ident, BinOp, UnaryOp, Call
    - TAstStmt-Hierarchie: VarDecl, Assign, If, While, Return, Block, ExprStmt
    - Top-Level: FuncDecl, ConDecl, Program
    - Enums: TAurumType (atInt64/atBool/atVoid/atPChar), TStorageKlass (skVar/skLet/skCo/skCon)
    - Hilfsfunktionen: AurumTypeToStr, StrToAurumType, NodeKindToStr
    - Saubere Speicherverwaltung: jeder Knoten gibt seine Kinder frei

---
+++ frontend/ast.pas
@@ -0,0 +1,696 @@
+{$mode objfpc}{$H+}
+unit ast;
+
+interface
+
+uses
+  SysUtils, Classes,
+  diag, lexer;
+
+type
+  { --- Aurum-Typsystem --- }
+
+  TAurumType = (atUnresolved, atInt64, atBool, atVoid, atPChar);
+
+  { --- Speicherklassen --- }
+
+  TStorageKlass = (skVar, skLet, skCo, skCon);
+
+  { --- Knotenarten (für schnellen Typcheck ohne 'is') --- }
+
+  TNodeKind = (
+    // Ausdrücke
+    nkIntLit, nkStrLit, nkBoolLit, nkIdent,
+    nkBinOp, nkUnaryOp, nkCall,
+    // Statements
+    nkVarDecl, nkAssign, nkIf, nkWhile, nkReturn,
+    nkBlock, nkExprStmt,
+    // Top-Level
+    nkFuncDecl, nkConDecl, nkProgram
+  );
+
+  { --- Vorwärtsdeklarationen --- }
+
+  TAstNode = class;
+  TAstExpr = class;
+  TAstStmt = class;
+
+  { --- Knotenlisten --- }
+
+  TAstNodeList = array of TAstNode;
+  TAstExprList = array of TAstExpr;
+  TAstStmtList = array of TAstStmt;
+
+  { --- Basisklasse --- }
+
+  TAstNode = class
+  private
+    FKind: TNodeKind;
+    FSpan: TSourceSpan;
+  public
+    constructor Create(aKind: TNodeKind; aSpan: TSourceSpan);
+    destructor Destroy; override;
+    property Kind: TNodeKind read FKind;
+    property Span: TSourceSpan read FSpan write FSpan;
+  end;
+
+  // ================================================================
+  // Ausdrücke
+  // ================================================================
+
+  TAstExpr = class(TAstNode)
+  private
+    FResolvedType: TAurumType;
+  public
+    constructor Create(aKind: TNodeKind; aSpan: TSourceSpan);
+    property ResolvedType: TAurumType read FResolvedType write FResolvedType;
+  end;
+
+  { Ganzzahl-Literal: 42 }
+  TAstIntLit = class(TAstExpr)
+  private
+    FValue: Int64;
+  public
+    constructor Create(aValue: Int64; aSpan: TSourceSpan);
+    property Value: Int64 read FValue;
+  end;
+
+  { String-Literal: "hello\n" }
+  TAstStrLit = class(TAstExpr)
+  private
+    FValue: string;
+  public
+    constructor Create(const aValue: string; aSpan: TSourceSpan);
+    property Value: string read FValue;
+  end;
+
+  { Bool-Literal: true / false }
+  TAstBoolLit = class(TAstExpr)
+  private
+    FValue: Boolean;
+  public
+    constructor Create(aValue: Boolean; aSpan: TSourceSpan);
+    property Value: Boolean read FValue;
+  end;
+
+  { Identifier-Referenz: x, counter }
+  TAstIdent = class(TAstExpr)
+  private
+    FName: string;
+  public
+    constructor Create(const aName: string; aSpan: TSourceSpan);
+    property Name: string read FName;
+  end;
+
+  { Binärer Operator: a + b, x == y }
+  TAstBinOp = class(TAstExpr)
+  private
+    FOp: TTokenKind;
+    FLeft: TAstExpr;
+    FRight: TAstExpr;
+  public
+    constructor Create(aOp: TTokenKind; aLeft, aRight: TAstExpr;
+      aSpan: TSourceSpan);
+    destructor Destroy; override;
+    property Op: TTokenKind read FOp;
+    property Left: TAstExpr read FLeft;
+    property Right: TAstExpr read FRight;
+  end;
+
+  { Unärer Operator: -x, !flag }
+  TAstUnaryOp = class(TAstExpr)
+  private
+    FOp: TTokenKind;
+    FOperand: TAstExpr;
+  public
+    constructor Create(aOp: TTokenKind; aOperand: TAstExpr;
+      aSpan: TSourceSpan);
+    destructor Destroy; override;
+    property Op: TTokenKind read FOp;
+    property Operand: TAstExpr read FOperand;
+  end;
+
+  { Funktionsaufruf: print_str("hi"), foo(1, 2) }
+  TAstCall = class(TAstExpr)
+  private
+    FName: string;
+    FArgs: TAstExprList;
+  public
+    constructor Create(const aName: string; const aArgs: TAstExprList;
+      aSpan: TSourceSpan);
+    destructor Destroy; override;
+    property Name: string read FName;
+    property Args: TAstExprList read FArgs;
+  end;
+
+  // ================================================================
+  // Statements
+  // ================================================================
+
+  TAstStmt = class(TAstNode)
+  public
+    constructor Create(aKind: TNodeKind; aSpan: TSourceSpan);
+  end;
+
+  { Variable/Let/Co-Deklaration: var x: int64 := 42; }
+  TAstVarDecl = class(TAstStmt)
+  private
+    FStorage: TStorageKlass;
+    FName: string;
+    FDeclType: TAurumType;
+    FInitExpr: TAstExpr;
+  public
+    constructor Create(aStorage: TStorageKlass; const aName: string;
+      aDeclType: TAurumType; aInitExpr: TAstExpr; aSpan: TSourceSpan);
+    destructor Destroy; override;
+    property Storage: TStorageKlass read FStorage;
+    property Name: string read FName;
+    property DeclType: TAurumType read FDeclType;
+    property InitExpr: TAstExpr read FInitExpr;
+  end;
+
+  { Zuweisung: x := expr; }
+  TAstAssign = class(TAstStmt)
+  private
+    FName: string;
+    FValue: TAstExpr;
+  public
+    constructor Create(const aName: string; aValue: TAstExpr;
+      aSpan: TSourceSpan);
+    destructor Destroy; override;
+    property Name: string read FName;
+    property Value: TAstExpr read FValue;
+  end;
+
+  { If-Statement: if (cond) thenStmt [else elseStmt] }
+  TAstIf = class(TAstStmt)
+  private
+    FCond: TAstExpr;
+    FThenBranch: TAstStmt;
+    FElseBranch: TAstStmt; // kann nil sein
+  public
+    constructor Create(aCond: TAstExpr; aThen: TAstStmt;
+      aElse: TAstStmt; aSpan: TSourceSpan);
+    destructor Destroy; override;
+    property Cond: TAstExpr read FCond;
+    property ThenBranch: TAstStmt read FThenBranch;
+    property ElseBranch: TAstStmt read FElseBranch;
+  end;
+
+  { While-Statement: while (cond) body }
+  TAstWhile = class(TAstStmt)
+  private
+    FCond: TAstExpr;
+    FBody: TAstStmt;
+  public
+    constructor Create(aCond: TAstExpr; aBody: TAstStmt;
+      aSpan: TSourceSpan);
+    destructor Destroy; override;
+    property Cond: TAstExpr read FCond;
+    property Body: TAstStmt read FBody;
+  end;
+
+  { Return-Statement: return [expr]; }
+  TAstReturn = class(TAstStmt)
+  private
+    FValue: TAstExpr; // kann nil sein (void-Funktionen)
+  public
+    constructor Create(aValue: TAstExpr; aSpan: TSourceSpan);
+    destructor Destroy; override;
+    property Value: TAstExpr read FValue;
+  end;
+
+  // Block: { stmt1; stmt2; ... }
+  TAstBlock = class(TAstStmt)
+  private
+    FStmts: TAstStmtList;
+  public
+    constructor Create(const aStmts: TAstStmtList; aSpan: TSourceSpan);
+    destructor Destroy; override;
+    property Stmts: TAstStmtList read FStmts;
+  end;
+
+  { Expression-Statement: expr; }
+  TAstExprStmt = class(TAstStmt)
+  private
+    FExpr: TAstExpr;
+  public
+    constructor Create(aExpr: TAstExpr; aSpan: TSourceSpan);
+    destructor Destroy; override;
+    property Expr: TAstExpr read FExpr;
+  end;
+
+  // ================================================================
+  // Top-Level Deklarationen
+  // ================================================================
+
+  { Funktionsparameter }
+  TAstParam = record
+    Name: string;
+    ParamType: TAurumType;
+    Span: TSourceSpan;
+  end;
+  TAstParamList = array of TAstParam;
+
+  // Funktionsdeklaration: fn name(params): retType { body }
+  TAstFuncDecl = class(TAstNode)
+  private
+    FName: string;
+    FParams: TAstParamList;
+    FReturnType: TAurumType;
+    FBody: TAstBlock;
+  public
+    constructor Create(const aName: string; const aParams: TAstParamList;
+      aReturnType: TAurumType; aBody: TAstBlock; aSpan: TSourceSpan);
+    destructor Destroy; override;
+    property Name: string read FName;
+    property Params: TAstParamList read FParams;
+    property ReturnType: TAurumType read FReturnType;
+    property Body: TAstBlock read FBody;
+  end;
+
+  { Con-Deklaration (Top-Level): con NAME: type := constExpr; }
+  TAstConDecl = class(TAstNode)
+  private
+    FName: string;
+    FDeclType: TAurumType;
+    FInitExpr: TAstExpr;
+  public
+    constructor Create(const aName: string; aDeclType: TAurumType;
+      aInitExpr: TAstExpr; aSpan: TSourceSpan);
+    destructor Destroy; override;
+    property Name: string read FName;
+    property DeclType: TAurumType read FDeclType;
+    property InitExpr: TAstExpr read FInitExpr;
+  end;
+
+  { Programm (Wurzelknoten): Liste von Top-Level Deklarationen }
+  TAstProgram = class(TAstNode)
+  private
+    FDecls: TAstNodeList;
+  public
+    constructor Create(const aDecls: TAstNodeList; aSpan: TSourceSpan);
+    destructor Destroy; override;
+    property Decls: TAstNodeList read FDecls;
+  end;
+
+{ --- Hilfsfunktionen --- }
+
+function AurumTypeToStr(t: TAurumType): string;
+function StrToAurumType(const s: string): TAurumType;
+function StorageKlassToStr(sk: TStorageKlass): string;
+function NodeKindToStr(nk: TNodeKind): string;
+
+implementation
+
+{ --- Hilfsfunktionen --- }
+
+function AurumTypeToStr(t: TAurumType): string;
+begin
+  case t of
+    atUnresolved: Result := '<unresolved>';
+    atInt64:      Result := 'int64';
+    atBool:       Result := 'bool';
+    atVoid:       Result := 'void';
+    atPChar:      Result := 'pchar';
+  end;
+end;
+
+function StrToAurumType(const s: string): TAurumType;
+begin
+  case s of
+    'int64': Result := atInt64;
+    'bool':  Result := atBool;
+    'void':  Result := atVoid;
+    'pchar': Result := atPChar;
+  else
+    Result := atUnresolved;
+  end;
+end;
+
+function StorageKlassToStr(sk: TStorageKlass): string;
+begin
+  case sk of
+    skVar: Result := 'var';
+    skLet: Result := 'let';
+    skCo:  Result := 'co';
+    skCon: Result := 'con';
+  end;
+end;
+
+function NodeKindToStr(nk: TNodeKind): string;
+begin
+  case nk of
+    nkIntLit:    Result := 'IntLit';
+    nkStrLit:    Result := 'StrLit';
+    nkBoolLit:   Result := 'BoolLit';
+    nkIdent:     Result := 'Ident';
+    nkBinOp:     Result := 'BinOp';
+    nkUnaryOp:   Result := 'UnaryOp';
+    nkCall:      Result := 'Call';
+    nkVarDecl:   Result := 'VarDecl';
+    nkAssign:    Result := 'Assign';
+    nkIf:        Result := 'If';
+    nkWhile:     Result := 'While';
+    nkReturn:    Result := 'Return';
+    nkBlock:     Result := 'Block';
+    nkExprStmt:  Result := 'ExprStmt';
+    nkFuncDecl:  Result := 'FuncDecl';
+    nkConDecl:   Result := 'ConDecl';
+    nkProgram:   Result := 'Program';
+  end;
+end;
+
+// ================================================================
+// TAstNode
+// ================================================================
+
+constructor TAstNode.Create(aKind: TNodeKind; aSpan: TSourceSpan);
+begin
+  inherited Create;
+  FKind := aKind;
+  FSpan := aSpan;
+end;
+
+destructor TAstNode.Destroy;
+begin
+  inherited Destroy;
+end;
+
+// ================================================================
+// TAstExpr
+// ================================================================
+
+constructor TAstExpr.Create(aKind: TNodeKind; aSpan: TSourceSpan);
+begin
+  inherited Create(aKind, aSpan);
+  FResolvedType := atUnresolved;
+end;
+
+// ================================================================
+// TAstIntLit
+// ================================================================
+
+constructor TAstIntLit.Create(aValue: Int64; aSpan: TSourceSpan);
+begin
+  inherited Create(nkIntLit, aSpan);
+  FValue := aValue;
+  FResolvedType := atInt64;
+end;
+
+// ================================================================
+// TAstStrLit
+// ================================================================
+
+constructor TAstStrLit.Create(const aValue: string; aSpan: TSourceSpan);
+begin
+  inherited Create(nkStrLit, aSpan);
+  FValue := aValue;
+  FResolvedType := atPChar;
+end;
+
+// ================================================================
+// TAstBoolLit
+// ================================================================
+
+constructor TAstBoolLit.Create(aValue: Boolean; aSpan: TSourceSpan);
+begin
+  inherited Create(nkBoolLit, aSpan);
+  FValue := aValue;
+  FResolvedType := atBool;
+end;
+
+// ================================================================
+// TAstIdent
+// ================================================================
+
+constructor TAstIdent.Create(const aName: string; aSpan: TSourceSpan);
+begin
+  inherited Create(nkIdent, aSpan);
+  FName := aName;
+end;
+
+// ================================================================
+// TAstBinOp
+// ================================================================
+
+constructor TAstBinOp.Create(aOp: TTokenKind; aLeft, aRight: TAstExpr;
+  aSpan: TSourceSpan);
+begin
+  inherited Create(nkBinOp, aSpan);
+  FOp := aOp;
+  FLeft := aLeft;
+  FRight := aRight;
+end;
+
+destructor TAstBinOp.Destroy;
+begin
+  FLeft.Free;
+  FRight.Free;
+  inherited Destroy;
+end;
+
+// ================================================================
+// TAstUnaryOp
+// ================================================================
+
+constructor TAstUnaryOp.Create(aOp: TTokenKind; aOperand: TAstExpr;
+  aSpan: TSourceSpan);
+begin
+  inherited Create(nkUnaryOp, aSpan);
+  FOp := aOp;
+  FOperand := aOperand;
+end;
+
+destructor TAstUnaryOp.Destroy;
+begin
+  FOperand.Free;
+  inherited Destroy;
+end;
+
+// ================================================================
+// TAstCall
+// ================================================================
+
+constructor TAstCall.Create(const aName: string;
+  const aArgs: TAstExprList; aSpan: TSourceSpan);
+begin
+  inherited Create(nkCall, aSpan);
+  FName := aName;
+  FArgs := aArgs;
+end;
+
+destructor TAstCall.Destroy;
+var
+  i: Integer;
+begin
+  for i := 0 to High(FArgs) do
+    FArgs[i].Free;
+  FArgs := nil;
+  inherited Destroy;
+end;
+
+// ================================================================
+// TAstStmt
+// ================================================================
+
+constructor TAstStmt.Create(aKind: TNodeKind; aSpan: TSourceSpan);
+begin
+  inherited Create(aKind, aSpan);
+end;
+
+// ================================================================
+// TAstVarDecl
+// ================================================================
+
+constructor TAstVarDecl.Create(aStorage: TStorageKlass;
+  const aName: string; aDeclType: TAurumType; aInitExpr: TAstExpr;
+  aSpan: TSourceSpan);
+begin
+  inherited Create(nkVarDecl, aSpan);
+  FStorage := aStorage;
+  FName := aName;
+  FDeclType := aDeclType;
+  FInitExpr := aInitExpr;
+end;
+
+destructor TAstVarDecl.Destroy;
+begin
+  FInitExpr.Free;
+  inherited Destroy;
+end;
+
+// ================================================================
+// TAstAssign
+// ================================================================
+
+constructor TAstAssign.Create(const aName: string; aValue: TAstExpr;
+  aSpan: TSourceSpan);
+begin
+  inherited Create(nkAssign, aSpan);
+  FName := aName;
+  FValue := aValue;
+end;
+
+destructor TAstAssign.Destroy;
+begin
+  FValue.Free;
+  inherited Destroy;
+end;
+
+// ================================================================
+// TAstIf
+// ================================================================
+
+constructor TAstIf.Create(aCond: TAstExpr; aThen: TAstStmt;
+  aElse: TAstStmt; aSpan: TSourceSpan);
+begin
+  inherited Create(nkIf, aSpan);
+  FCond := aCond;
+  FThenBranch := aThen;
+  FElseBranch := aElse;
+end;
+
+destructor TAstIf.Destroy;
+begin
+  FCond.Free;
+  FThenBranch.Free;
+  FElseBranch.Free; // nil.Free ist sicher in FPC
+  inherited Destroy;
+end;
+
+// ================================================================
+// TAstWhile
+// ================================================================
+
+constructor TAstWhile.Create(aCond: TAstExpr; aBody: TAstStmt;
+  aSpan: TSourceSpan);
+begin
+  inherited Create(nkWhile, aSpan);
+  FCond := aCond;
+  FBody := aBody;
+end;
+
+destructor TAstWhile.Destroy;
+begin
+  FCond.Free;
+  FBody.Free;
+  inherited Destroy;
+end;
+
+// ================================================================
+// TAstReturn
+// ================================================================
+
+constructor TAstReturn.Create(aValue: TAstExpr; aSpan: TSourceSpan);
+begin
+  inherited Create(nkReturn, aSpan);
+  FValue := aValue;
+end;
+
+destructor TAstReturn.Destroy;
+begin
+  FValue.Free;
+  inherited Destroy;
+end;
+
+// ================================================================
+// TAstBlock
+// ================================================================
+
+constructor TAstBlock.Create(const aStmts: TAstStmtList;
+  aSpan: TSourceSpan);
+begin
+  inherited Create(nkBlock, aSpan);
+  FStmts := aStmts;
+end;
+
+destructor TAstBlock.Destroy;
+var
+  i: Integer;
+begin
+  for i := 0 to High(FStmts) do
+    FStmts[i].Free;
+  FStmts := nil;
+  inherited Destroy;
+end;
+
+// ================================================================
+// TAstExprStmt
+// ================================================================
+
+constructor TAstExprStmt.Create(aExpr: TAstExpr; aSpan: TSourceSpan);
+begin
+  inherited Create(nkExprStmt, aSpan);
+  FExpr := aExpr;
+end;
+
+destructor TAstExprStmt.Destroy;
+begin
+  FExpr.Free;
+  inherited Destroy;
+end;
+
+// ================================================================
+// TAstFuncDecl
+// ================================================================
+
+constructor TAstFuncDecl.Create(const aName: string;
+  const aParams: TAstParamList; aReturnType: TAurumType;
+  aBody: TAstBlock; aSpan: TSourceSpan);
+begin
+  inherited Create(nkFuncDecl, aSpan);
+  FName := aName;
+  FParams := aParams;
+  FReturnType := aReturnType;
+  FBody := aBody;
+end;
+
+destructor TAstFuncDecl.Destroy;
+begin
+  FBody.Free;
+  FParams := nil;
+  inherited Destroy;
+end;
+
+// ================================================================
+// TAstConDecl
+// ================================================================
+
+constructor TAstConDecl.Create(const aName: string;
+  aDeclType: TAurumType; aInitExpr: TAstExpr; aSpan: TSourceSpan);
+begin
+  inherited Create(nkConDecl, aSpan);
+  FName := aName;
+  FDeclType := aDeclType;
+  FInitExpr := aInitExpr;
+end;
+
+destructor TAstConDecl.Destroy;
+begin
+  FInitExpr.Free;
+  inherited Destroy;
+end;
+
+// ================================================================
+// TAstProgram
+// ================================================================
+
+constructor TAstProgram.Create(const aDecls: TAstNodeList;
+  aSpan: TSourceSpan);
+begin
+  inherited Create(nkProgram, aSpan);
+  FDecls := aDecls;
+end;
+
+destructor TAstProgram.Destroy;
+var
+  i: Integer;
+begin
+  for i := 0 to High(FDecls) do
+    FDecls[i].Free;
+  FDecls := nil;
+  inherited Destroy;
+end;
+
+end.
